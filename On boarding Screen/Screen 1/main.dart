54b15248d4e21092639d1fbdedc4f78099536918
Tarikul Islam Shykat <96073081+helper-bud@users.noreply.github.com>
2024-05-08 15:30:24 +0600

    Create main.dart

---
+++ On boarding Screen/Screen 1/main.dart
@@ -0,0 +1,14 @@
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Time Input Example',
+      home: OnBoardingScreen_1(),
+    );
+  }
+}
